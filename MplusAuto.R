24c64eec1dce02a3a4a584eafc00d40cf75f9ab4
ccwang-ubuntu <abelardccwang@gmail.com>
2018-10-20 00:31:35 +0900

    done test

--- MplusAuto.R
+++ MplusAuto.R
@@ -6,7 +6,7 @@ library(MplusAutomation)
 
 
 
-prepareMplusData(ex3_7, "data/test.dat")
+# prepareMplusData(ex3_7, "data/test.dat")
 runModels("ex3.5.inp", showOutput = TRUE, Mplus_command = "/opt/mplus/8.1/mplus")
 
 # Read model 
